7cd689393556be311a3c57c131b34e57387559b6
mohsabkha <mohsabkha@gmail.com>
2022-06-16 13:56:53 -0500

    fixed card functionality

--- youre-going-daon/src/components/card/card.js
+++ youre-going-daon/src/components/card/card.js
@@ -3,8 +3,23 @@ import React, { useEffect } from 'react'
 function Card(props) {
   return (
     <div hidden={props.hiddenFlag}>
-        {props.info}
-        {console.log("CARD: ", props.hiddenFlag)}
+      {console.log(props.info)}
+      { props.info ? <div>
+            <h1>{props.info.projectName}</h1>   
+            <p>{props.info.projectDescription}</p>   
+            <h2>Team Members</h2>
+            <div>
+              {
+              props.info.teamMembers.map((el) => {
+                return <div>
+                  <p>Name: {el.name} </p>
+                  <p hidden> {el.id} </p>
+                </div>
+              })}
+            </div>
+          </div>
+          : null 
+        }
     </div>
   )
 }
--- youre-going-daon/src/components/list/list.js
+++ youre-going-daon/src/components/list/list.js
@@ -4,7 +4,12 @@ import { useState } from 'react';
 
 function List() {
     //pull api data here
-    let myList = ['hello', 'im', 'a', 'list'];
+    let myMockJson = {"data" :
+        [
+            {"teamId" : 12345, "projectName" : "Project Name 1", "projectDescription": "This is the description of project 1", "teamMembers" : [{"name": "David", "id" : 12345}, {"name": "Sabir", "id": 54321}]}, 
+            {"teamId" : 54321, "projectName" : "Project Name 2", "projectDescription": "This is the description of project 2", "teamMembers" : [{"name": "Insert Name", "id" : 12345}, {"name": "Insert Name", "id": 54321}]}
+        ]
+    };
     const [hiddenFlag, updateHiddenFlag] = useState(true);
     const [selectedData, updateData] = useState();
     const [selectedIndex, updateIndex] = useState();
@@ -13,12 +18,12 @@ function List() {
         updateIndex(index);
     }
 
-    const logic = (currIndex) => {
+    const logic = (currIndex, el) => {
         if (selectedIndex === currIndex && (hiddenFlag || !hiddenFlag)) {
             updateHiddenFlag(!hiddenFlag)
             console.log("case 1: ", currIndex)
         } else if (selectedIndex != currIndex) {
-            updateState(myList[currIndex], currIndex)
+            updateState(el, currIndex)
             if (hiddenFlag) {
                 updateHiddenFlag(!hiddenFlag)
             } 
@@ -27,10 +32,10 @@ function List() {
     }
     return (
             <div>
-            {myList.map((el, index)=>{
-                return <div onClick={()=>{logic(index)}}>
-                    {"\n" + el}
-                    
+                {myMockJson.data.map((el, index)=>{
+                return <div onClick={()=>{logic(index, el)}}>
+                        <h1>{el.projectName}</h1>   
+                        <p>{el.projectDescription}</p> 
                 </div>
             })}
             <Card hiddenFlag={hiddenFlag} info={selectedData}></Card>
