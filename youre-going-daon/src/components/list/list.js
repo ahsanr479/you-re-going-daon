29a52f7bcc1e0aed2d60c1fda122a7768c1ef674
mohsabkha <mohsabkha@gmail.com>
2022-06-16 11:07:14 -0500

    added button

--- youre-going-daon/src/components/list/list.js
+++ youre-going-daon/src/components/list/list.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
 function List() {
+    //pull api data here
     let myList = ['hello', 'im', 'a', 'list'];
     return (
         <div>
             {myList.map((el)=>{
-                return <div>{"\n" + el}</div>
+                return <a href='#' onClick={()=>{alert('You Clicked Me!')}}>
+                    {"\n" + el}
+                </a>
             })}
+            
         </div>
     )
 }
