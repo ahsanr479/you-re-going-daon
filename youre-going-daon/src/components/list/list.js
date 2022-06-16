5ab68e75157d36d2e482337a2710e3a800e0e826
mohsabkha <mohsabkha@gmail.com>
2022-06-16 12:38:49 -0500

    doing some things with the stuff

--- youre-going-daon/src/components/list/list.js
+++ youre-going-daon/src/components/list/list.js
@@ -5,21 +5,36 @@ import { useState } from 'react';
 function List() {
     //pull api data here
     let myList = ['hello', 'im', 'a', 'list'];
-    let data={}
     const [hiddenFlag, updateHiddenFlag] = useState(true);
+    const [selectedData, updateData] = useState();
+    const [selectedIndex, updateIndex] = useState();
+    const updateState = (data, index) => {
+        updateData(data);
+        updateIndex(index);
+    }
 
+    const logic = (currIndex) => {
+        if (selectedIndex === currIndex && (hiddenFlag || !hiddenFlag)) {
+            updateHiddenFlag(!hiddenFlag)
+            console.log("case 1: ", currIndex)
+        } else if (selectedIndex != currIndex) {
+            updateState(myList[currIndex], currIndex)
+            if (hiddenFlag) {
+                updateHiddenFlag(!hiddenFlag)
+            } 
+            console.log("case 2: ", currIndex)
+        } 
+    }
     return (
             <div>
-            {myList.map((el)=>{
-                return <div onClick={()=>{updateHiddenFlag(!hiddenFlag); console.log("rendering: ", hiddenFlag)}}>
+            {myList.map((el, index)=>{
+                return <div onClick={()=>{logic(index)}}>
                     {"\n" + el}
                     
                 </div>
             })}
-            <Card hiddenFlag={hiddenFlag} info="example text" data={data}></Card>
+            <Card hiddenFlag={hiddenFlag} info={selectedData}></Card>
         </div>
-
-        
     )
 }
 
